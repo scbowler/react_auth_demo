f78ef97922d74e4027f0ac2bd1a40ff41831af54
Scott Bowler <scott.bowler@learningfuze.com>
2019-12-04 20:33:48 -0800

    Completed sign up form

--- src/components/sign_up.js
+++ src/components/sign_up.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import Input from './input';
 
 class SignUp extends React.Component {
     userSignUp(formValues){
@@ -15,17 +16,16 @@ class SignUp extends React.Component {
 
                 <form onSubmit={handleSubmit(this.userSignUp)}>
                     <div className="row">
-                        <div className="col s12 input-field">
-                            <Field type="text" name="Name" component="input"/>
-                            <label>Name</label>
-                        </div>
+                        <Field size="m6 s12" name="name" component={Input} label="Enter your name" />
+
+                        <Field size="m6 s12"  name="email" component={Input} label="Enter your email" />
                     </div>
                     <div className="row">
-                        <div className="col s12 input-field">
-                            <Field type="text" name="Email" component="input"/>
-                            <label>Email</label>
-                        </div>
+                        <Field size="m6 s12" name="password" component={Input} label="Choose a password" type="password" />
+
+                        <Field size="m6 s12" name="confirmPassword" component={Input} label="Confirm password" type="password" />
                     </div>
+
                     <div className="row right">
                         <button className="btn blue">Create Account</button>
                     </div>
@@ -35,6 +35,29 @@ class SignUp extends React.Component {
     }
 }
 
+function validate({ name, email, password, confirmPassword }) {
+    const errors = {};
+
+    if(!name) {
+        errors.name = 'Please enter your name';
+    }
+
+    if(!email) {
+        errors.email = 'Please enter your email address';
+    }
+
+    if(!password) {
+        errors.password = 'Please choose a password';
+    }
+
+    if(password !== confirmPassword) {
+        errors.confirmPassword = 'Passwords do not match';
+    }
+
+    return errors;
+}
+
 export default reduxForm({
-    form: 'sign-up'
+    form: 'sign-up',
+    validate: validate
 })(SignUp);
